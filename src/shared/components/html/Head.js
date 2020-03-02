f2f0193e6c9ed42352e6e68679b6f48296ba08fe
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-03-02 18:27:06 -0500

    added .html to canonical links to preserve google data

--- src/shared/components/html/Head.js
+++ src/shared/components/html/Head.js
@@ -3,14 +3,14 @@ import { Helmet } from 'react-helmet'
 import { string } from 'prop-types'
 
 const Head = ({ path, title, alternate, noindex }) => {
-  const fixedPath = path === '/reel' ? '' : path
+  const fixedPath = path === '/reel' ? '' : `${path}.html`
 
   return (
     <Helmet>
       <title>{title}</title>
       {noindex
         ? <meta name="robots" content="noindex, nofollow"/>
-        : <link rel="canonical" href={`https://www.crismertens.com${fixedPath}.html`} />}
+        : <link rel="canonical" href={`https://www.crismertens.com${fixedPath}`} />}
       {!noindex && alternate && <link rel="alternate" href={`https://www.crismertens.com${alternate}`} />}
     </Helmet>
   )
