7842626f99266d9ab77df95a5f29be5077ca66e6
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-03-05 17:01:10 -0500

    added children prop for custom head tags

--- src/shared/components/html/Head.js
+++ src/shared/components/html/Head.js
@@ -2,16 +2,17 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 import { string, bool } from 'prop-types'
 
-const Head = ({ path, title, alternate, noindex }) => {
-  const fixedPath = path === '/reel' ? '' : `${path}.html`
+const Head = ({ path, title, alternate, noindex, children }) => {
+  const fixedPath = path === '/reel' ? '' : path
 
   return (
-    <Helmet>
+    <Helmet encodeSpecialCharacters={false}>
       <title>{title}</title>
       {noindex
         ? <meta name="robots" content="noindex, nofollow"/>
         : <link rel="canonical" href={`https://www.crismertens.com${fixedPath}`} />}
       {!noindex && alternate && <link rel="alternate" href={`https://www.crismertens.com${alternate}`} />}
+      { children }
     </Helmet>
   )
 }
